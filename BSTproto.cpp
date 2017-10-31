77b31acee01a62594d3eb37fa79988cea245574e
David Gonzalez <shadowslightg@hotmail.com>
2017-10-31 13:57:06 -0700

    Update BSTproto.cpp

--- BSTproto.cpp
+++ BSTproto.cpp
@@ -6,8 +6,18 @@
 #include <cstdlib>
 
 #include "BST.cpp"
-
 using namespace std;
+
+
+
+struct Movies_Bio
+{
+	string Movie Title;
+	int Year;
+	char Actors[];
+};
+
+typedef Movies_Bio info; 
 
 int main()
 {
